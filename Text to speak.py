24eb1d771c50f903c117e15b8139f8f4a8fca64f
rishigi <153375333+rishigi@users.noreply.github.com>
2024-01-30 18:45:28 +0530

    Delete Text to speech.py
    How I made this ?
    1) First I read about win32com on chrome and how to use it .Then I install win32com, which is a module used for text to speech. "pip install pywin32". 
    2)After reading and downloading module i started my code from importing module. Then i used while loop for continues input from user.
    3)At the last i used if condition to stop program.
    
    What I learn from this project ?
    First I learn that how to  know about any module and how to download and use. Then after  I learn about while loop and if condition.
    
    SO THIS IS FIRST PROJECT IN PYTHON AND I AM UPLOADING MY FIRST PROJECT IN MY GITHUB REPO FOR FIRST TIME.

--- Text to speak.py
+++
@@ -1,9 +0,0 @@
-import win32com.client as win32
-
-speaker = win32.Dispatch("SAPI.SpVoice")
-
-while True:
-    s = input("entre what you want me to speak: ")
-    speaker.Speak(s)
-    if (s)=="q":
-        break
